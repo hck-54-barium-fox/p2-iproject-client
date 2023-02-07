93328cfa9a827a4a96bed47194237269a26e8bf8
redaffa <redaffaashshiddiq@gmail.com>
2023-02-07 13:59:00 +0700

    added login feature

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,3 +1,5 @@
+const { comparePassword } = require("../helpers/bcrypt")
+const { signToken } = require("../helpers/jwt")
 const {User} = require("../models/index")
 class UserController{
 
@@ -13,6 +15,35 @@ class UserController{
                 res.status(400).json({message:err.errors[0].message})
             }else{
                 res.status(500).json(err)
+            }
+        }
+    }
+    static async loginUser(req,res){
+        try {
+            const{email,password} = req.body
+            let user = await User.findOne({
+                where:{
+                    email
+                }
+            })
+            if(!user){
+                throw({status:400,message:"Invalid email/password"})
+            }
+            let comparedPassword = comparePassword(password,user.password)
+            if(!comparedPassword){
+                throw({status:400,message:"Invalid email/password"})
+            }
+            let access_token = signToken({
+                id:user.id,
+                email:user.email
+            })
+            res.status(200).json({access_token})
+        } catch (err) {
+            console.log(err)
+            if(err.status){
+                res.status(err.status).json({message:err.message})
+            }else{
+                res.status(500).json({message:"Internal server error"})
             }
         }
     }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/UserController')
 const router = express.Router()
 
 router.post("/register",UserController.registerUser)
+router.post("/login",UserController.loginUser)
 
 module.exports = router
