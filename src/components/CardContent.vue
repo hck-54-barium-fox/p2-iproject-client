dde2435cbb8b0f5e1ee6f62582f3b4b165078538
aprizallahmad <aprizallahmad@gmail.com>
2023-02-08 06:16:58 +0700

    feat: set up home

---
+++ src/components/CardContent.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<template>
+
+<div class="group">
+          <div class="relative">
+            <div class="w-full">
+              <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/abaddon.png" class="w-full h-full object-center object-cover opacity-70 group-hover:opacity-100" />
+              <div class="absolute bottom-0 px-2 py-9 h-16 flex items-center bg-gradient-to-t from-black w-full">
+                <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png" />
+                <p class="text-lg text-white uppercase inline-block align-start text-left pl-2">Abaddon</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center items-center h-16">
+            <button class="px-3 py-2 text-gray-900 bg-gray-100 rounded-sm focus:outline-none focus:ring focus:ring-gray-500 uppercase tracking-widest">Pick Hero</button>
+          </div>
+        </div>
+
+        <!-- HeroesCard -->
+        <div class="group">
+          <div class="relative">
+            <div class="w-full overflow-hidden">
+              <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/arc_warden.png" class="w-full h-full object-center object-cover opacity-70 group-hover:opacity-100" />
+              <div class="absolute bottom-0 px-2 py-9 h-16 flex items-center bg-gradient-to-t from-black w-full">
+                <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png" />
+                <p class="text-lg text-white uppercase inline-block align-start text-left pl-2">Arc Warden</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center items-center h-16">
+            <button class="px-3 py-2 text-gray-900 bg-gray-100 rounded-sm focus:outline-none focus:ring focus:ring-gray-500 uppercase tracking-widest">Pick Hero</button>
+          </div>
+        </div>
+
+        <!-- HeroesCard -->
+        <div class="group">
+          <div class="relative">
+            <div class="w-full overflow-hidden">
+              <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/brewmaster.png" class="w-full h-full object-center object-cover opacity-70 group-hover:opacity-100" />
+              <div class="absolute bottom-0 px-2 py-9 h-16 flex items-center bg-gradient-to-t from-black w-full">
+                <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png" />
+                <p class="text-lg text-white uppercase inline-block align-start text-left pl-2">Brewmaster</p>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-center items-center h-16">
+            <button class="px-3 py-2 text-gray-900 bg-gray-100 rounded-sm focus:outline-none focus:ring focus:ring-gray-500 uppercase tracking-widest">Pick Hero</button>
+          </div>
+        </div>
+</template>
