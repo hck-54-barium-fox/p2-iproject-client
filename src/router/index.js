d769976325286e5b2524cb82b165eaaf950bcee7
MrBista <gustibisman2308@gmail.com>
2023-02-07 16:19:14 +0700

    finish register page template

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import LoginPage from '../views/LoginPage.vue';
+import RegisterPage from '../views/RegisterPage.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,6 +12,10 @@ const router = createRouter({
     {
       path: '/login',
       component: LoginPage,
+    },
+    {
+      path: '/register',
+      component: RegisterPage,
     },
   ],
 });
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,71 @@
+<script>
+
+</script>
+
+<template>
+ <div class="lg:flex">
+  <div class="hidden lg:flex items-center justify-center bg-sky-100 flex-1 h-screen">
+   <div class="max-w-7xl transform duration-200 hover:scale-110 cursor-pointer mix-blend-multiply">
+    <img
+     src="https://img.freepik.com/free-vector/electrician-service_335657-3186.jpg?w=1060&t=st=1675732520~exp=1675733120~hmac=29782079b38be4c5aa4b82ea5ac09d9f10c348e3a5131092e52378ed3b5bc02b"
+     alt="dashboard" class="w-[100%] mx-auto" />
+   </div>
+  </div>
+  <div class="lg:w-1/2 xl:max-w-screen-sm">
+   <div class="py-12 bg-sky-100 lg:bg-white flex justify-center lg:justify-start lg:px-12">
+    <div class="cursor-pointer flex items-center">
+     <div></div>
+     <div class="text-2xl text-sky-800 tracking-wide ml-2 font-semibold">
+      Ya Boking
+     </div>
+    </div>
+   </div>
+   <div class="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
+    <h2
+     class="text-center text-4xl text-sky-900 font-display font-semibold lg:text-left xl:text-5xl xl:text-bold mt-20">
+     Register
+    </h2>
+    <div class="mt-12 relative">
+     <form @submit.prevent="login">
+      <div>
+       <div class="text-sm font-bold text-gray-700 tracking-wide">
+        Email Address
+       </div>
+       <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500" type="email"
+        placeholder="name@gmail.com" name="email" />
+      </div>
+      <div class="mt-8">
+       <div class="text-sm font-bold text-gray-700 tracking-wide">
+        name
+       </div>
+       <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500" type="email"
+        placeholder="eg. bambang" name="email" />
+      </div>
+      <div class="mt-8">
+       <div class="flex justify-between items-center">
+        <div class="text-sm font-bold text-gray-700 tracking-wide">
+         Password
+        </div>
+
+       </div>
+       <input class="w-full text-lg py-2 border-b border-gray-300 focus:outline-none focus:border-sky-500"
+        type="password" name="password" placeholder="Enter your password" />
+      </div>
+      <div class="mt-10">
+       <button
+        class="bg-sky-500 text-gray-100 p-4 w-full rounded-full tracking-wide font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-sky-600 shadow-lg"
+        type="submit">
+        Register
+       </button>
+      </div>
+     </form>
+     <div class="mt-12 text-sm font-display font-semibold text-gray-700 text-center">
+      Already have an account ?
+      <a @click.prevent="$router.push('/register')" class="cursor-pointer text-sky-600 hover:text-sky-800">Sign
+      </a>
+     </div>
+    </div>
+   </div>
+  </div>
+ </div>
+</template>
