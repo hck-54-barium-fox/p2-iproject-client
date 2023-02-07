94c89d91433c06ac56a99381e7236fc3ba8c8de3
MrBista <gustibisman2308@gmail.com>
2023-02-07 20:03:43 +0700

    done functionality login

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -9,6 +9,16 @@ export const useBookingStore = defineStore('booking', {
       try {
         const { data } = await axios.post(baseUrl + '/register', form);
         console.log(data, 'ini data');
+        this.router.push('/login');
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async handleLogin(form) {
+      try {
+        const { data } = await axios.post(baseUrl + '/login', form);
+        console.log(data);
+        localStorage.setItem('access_token', data.access_token);
         this.router.push('/');
       } catch (err) {
         console.log(err);
