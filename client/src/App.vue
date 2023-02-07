af1dab43cf452529c7aa315ae2c573f51b027520
Hamzah Diza Santoso <hamzah.diza@gmail.com>
2023-02-07 23:15:53 +0700

    Navbar

---
+++ client/src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import Navbar from "./components/Navbar.vue";
+import { RouterLink, RouterView } from "vue-router";
+import { mapActions, mapWritableState } from "pinia";
+import { useAppStore } from "./stores/appStore";
+export default {
+  components: {
+    Navbar,
+  },
+  computed: {
+    ...mapWritableState(useAppStore, ["isLogin"]),
+  },
+  methods: {
+    ...mapActions(useAppStore, ["isLoginToggle"]),
+  },
+  created() {
+    if (localStorage.access_token) {
+      this.isLogin = true;
+    } else {
+      this.isLogin = false;
+    }
+  },
+};
+</script>
+
+<template>
+  <Navbar />
+
+  <router-view></router-view>
+</template>
+
+<style></style>
