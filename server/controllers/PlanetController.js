b5e7ef96ede7f8495b47f25a12de1f333bad42eb
redaffa <redaffaashshiddiq@gmail.com>
2023-02-07 15:42:19 +0700

    feat: -added readPlanetById endpoint and controller

--- server/controllers/PlanetController.js
+++ server/controllers/PlanetController.js
@@ -10,9 +10,25 @@ class PlanetController{
                     'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
                   }
             })
-            console.log(data)
             res.status(200).json(data)
         } catch (err) {
+            res.status(500).json(err)
+        }
+    }
+    static async readPlanetById(req,res){
+        try {
+            let keyId = req.params.id
+            let {data} = await axios({
+                method:'GET',
+                url:`https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/${keyId}`,
+                headers: {
+                    'X-RapidAPI-Key': '0e0c5bb78amshed1b6b5d96fd136p1de1c2jsn583fcac5966d',
+                    'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
+                  }
+            })
+            res.status(200).json(data)
+        } catch (err) {
+            console.log(err)
             res.status(500).json(err)
         }
     }
--- server/routes/planet.js
+++ server/routes/planet.js
@@ -3,5 +3,6 @@ const PlanetController = require('../controllers/PlanetController')
 const router = express.Router()
 
 router.get("/planets",PlanetController.readPlanets)
-
+router.get("/planets/:id",PlanetController.readPlanetById)
+router.get("/apod",PlanetController.getAPOD)
 module.exports= router
