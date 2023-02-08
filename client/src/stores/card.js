6d4d83949e87b3a94b3ce44633537a108b1a0bb1
eduardusoldi <eduardusoldi98@gmail.com>
2023-02-08 10:38:08 +0700

    first setup

---
+++ client/src/stores/card.js
@@ -0,0 +1,13 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCardStore = defineStore({
+  id: "card",
+  state: () => {
+    return {
+    };
+  },
+  getters: {},
+  actions: {}
+});
+
