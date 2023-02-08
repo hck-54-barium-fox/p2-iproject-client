e99691e16631d43005c421c438e3ed935d52e50e
redaffa <redaffaashshiddiq@gmail.com>
2023-02-08 15:30:13 +0700

    -delete server

--- server/routes/planet.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const PlanetController = require('../controllers/PlanetController')
-const router = express.Router()
-
-router.get("/planets",PlanetController.readPlanets)
-router.get("/planets/:id",PlanetController.readPlanetById)
-router.get("/apod",PlanetController.getAPOD)
-module.exports= router
--- server/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const UserController = require('../controllers/UserController')
-const router = express.Router()
-
-router.post("/register",UserController.registerUser)
-router.post("/login",UserController.loginUser)
-
-module.exports = router
