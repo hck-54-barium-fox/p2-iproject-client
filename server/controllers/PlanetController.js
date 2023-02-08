e43c277fd6cc3a95d612ae79ac30b1426ffcb12d
redaffa <redaffaashshiddiq@gmail.com>
2023-02-08 13:36:26 +0700

    refactor: -changing the output on readPlanets

--- server/controllers/PlanetController.js
+++ server/controllers/PlanetController.js
@@ -10,6 +10,18 @@ class PlanetController{
                     'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
                   }
             })
+            data = data.map(el=>{
+                return {
+                    id:el.id,
+                    name : el.name,
+                    description : el.description,
+                    img:{
+                        imgUrl:el.imgSrc[0].img,
+                        imgDescription:el.imgSrc[0].imgDescription
+                    },
+                    keyId:el.key
+                }
+            })
             res.status(200).json(data)
         } catch (err) {
             res.status(500).json(err)
@@ -25,6 +37,18 @@ class PlanetController{
                     'X-RapidAPI-Key': '0e0c5bb78amshed1b6b5d96fd136p1de1c2jsn583fcac5966d',
                     'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
                   }
+            })
+            data = data.map(el=>{
+                return {
+                    id:el.id,
+                    name : el.name,
+                    description : el.description,
+                    img:{
+                        imgUrl:el.imgSrc[0].img,
+                        imgDescription:el.imgSrc[0].imgDescription
+                    },
+                    keyId:el.key
+                }
             })
             res.status(200).json(data)
         } catch (err) {
