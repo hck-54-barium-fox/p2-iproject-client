863d3fe3d542df27127fdcf200904ebd834ec782
yugamauludi <yugamauludi98@gmail.com>
2023-02-08 13:14:27 +0700

    regist login done

---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,165 @@
+<script>
+
+</script>
+
+<template>
+    <!--home-->
+<div class="container" style="margin-top:50px;">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card-sl">
+        <div class="card-image">
+          <img
+            src="https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+        </div>
+          <a class="card-action" href="#"><i class="fa fa-heart"></i></a>
+          <div class="card-heading">
+            Audi Q8
+          </div>
+          <div class="card-text">
+            Audi Q8 is a full-size luxury crossover SUV coupé made by Audi that was launched in 2018.
+          </div>
+          <div class="card-text">
+            $67,400
+          </div>
+        <a href="#" class="card-button"> Purchase</a>
+        <a href="#" class="card-button2"> Detail</a>
+
+      </div>
+    </div>
+    <div class="col-md-3">
+      <div class="card-sl">
+        <div class="card-image">
+          <img
+            src="https://images.pexels.com/photos/1149831/pexels-photo-1149831.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+        </div>
+          <a class="card-action" href="#"><i class="fa fa-heart"></i></a>
+          <div class="card-heading">
+            Audi Q8
+          </div>
+          <div class="card-text">
+            Audi Q8 is a full-size luxury crossover SUV coupé made by Audi that was launched in 2018.
+          </div>
+          <div class="card-text">
+            $67,400
+          </div>
+        <a href="#" class="card-button"> Purchase</a>
+        <a href="#" class="card-button2"> Detail</a>
+      </div>
+    </div>
+  </div>  
+</div>  
+</template>
+
+<style>
+    /* stylingCard */
+  /*  Helper Styles */
+  body {
+    font-family: Varela Round;
+    background: #f1f1f1;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  /* Card Styles */
+
+  .card-sl {
+    border-radius: 8px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  .card-image img {
+    max-height: 100%;
+    max-width: 100%;
+    border-radius: 8px 8px 0px 0;
+  }
+
+  .card-action {
+    position: relative;
+    float: right;
+    margin-top: -25px;
+    margin-right: 20px;
+    z-index: 2;
+    color: #E26D5C;
+    background: #fff;
+    border-radius: 100%;
+    padding: 15px;
+    font-size: 15px;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 1px 2px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  .card-action:hover {
+    color: #fff;
+    background: #E26D5C;
+    -webkit-animation: pulse 1.5s infinite;
+  }
+
+  .card-heading {
+    font-size: 18px;
+    font-weight: bold;
+    background: #fff;
+    padding: 10px 15px;
+  }
+
+  .card-text {
+    padding: 10px 15px;
+    background: #fff;
+    font-size: 14px;
+    color: #636262;
+  }
+
+  .card-button2 {
+    display: flex;
+    justify-content: center;
+    padding: 10px 0;
+    width: 100%;
+    background-color: #1F487E;
+    color: #fff;
+    border-radius: 0 0 10px 10px;
+  }
+
+  .card-button {
+    display: flex;
+    justify-content: center;
+    padding: 10px 0;
+    width: 100%;
+    background-color: #1F487E;
+    color: #fff;
+  }
+
+  .card-button:hover {
+    text-decoration: none;
+    background-color: #1D3461;
+    color: #fff;
+
+  }
+
+
+  @-webkit-keyframes pulse {
+    0% {
+      -moz-transform: scale(0.9);
+      -ms-transform: scale(0.9);
+      -webkit-transform: scale(0.9);
+      transform: scale(0.9);
+    }
+
+    70% {
+      -moz-transform: scale(1);
+      -ms-transform: scale(1);
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      box-shadow: 0 0 0 50px rgba(90, 153, 212, 0);
+    }
+
+    100% {
+      -moz-transform: scale(0.9);
+      -ms-transform: scale(0.9);
+      -webkit-transform: scale(0.9);
+      transform: scale(0.9);
+      box-shadow: 0 0 0 0 rgba(90, 153, 212, 0);
+    }
+  }
+  /* endStylingCard */
+</style>
