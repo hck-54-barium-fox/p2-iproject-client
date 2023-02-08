c3cd41dce05318b78cc7dd31266d00d869fee234
eduardusoldi <eduardusoldi98@gmail.com>
2023-02-09 04:37:49 +0700

    feat: handle fetchMyDeck, remove card from deck, and add card to deck

--- client/src/components/MyCard.vue
+++ client/src/components/MyCard.vue
@@ -1,23 +1,16 @@
 <script>
-// import { mapActions, mapState, mapWritableState } from 'pinia';
+import { mapActions, mapState, mapWritableState } from 'pinia';
+import { useCardStore } from '../stores/card';
 
 export default {
-    // props: ['food'],
-    // methods: {
-    //     ...mapActions(useFoodStore, ['addWishlist', 'getFoodById',]),
-    //     addingNewWishlist(id) {
-    //         this.addWishlist(id)
-    //         this.foodId.push(id)
-    //     },
-    //     getDetail(id){
-    //         this.$router.push(`/${id}`)
-
-    //     }
-
-    // },
-    // computed: {
-    //     ...mapWritableState(useFoodStore, ['foodId', 'foodDetailId', 'qrCode'])
-    // }
+    props: ['card'],
+    methods: {
+        ...mapActions(useCardStore, ['deleteCard', 'fetchMyDeck']),
+        removeCard(id){
+            this.deleteCard(id)
+            this.fetchMyDeck()
+        }
+    }
 }
 
 </script>
@@ -26,24 +19,18 @@ export default {
     <!-- Card 1 -->
     <div class="card pt-3 shadow p-3 mb-5 rounded" style="width: 18rem;">
         <div class="card-header fst-italic">
-           <img src="https://www.pngkey.com/png/full/362-3629652_5-elixir-de-clash-of-clans.png"
-           style="width: 35px"
-           /> Elixir Cost: 3
+            <img src="https://www.pngkey.com/png/full/362-3629652_5-elixir-de-clash-of-clans.png" style="width: 35px" />
+            Elixir Cost: {{ card.Card.elixir }}
         </div>
-        <!-- <div class="d-flex flex-row-reverse">
-            <div v-if="foodId.some(el => el === food.id)"
-                class="p-2"
-                @click.prevent="addingNewWishlist(food.id)"><i class="bi bi-bookmark-fill"></i>
-            </div>
-            <div v-else>
-                <div @click.prevent="addingNewWishlist(food.id)" class="p-2"><i class="bi bi-bookmark"></i>
-                </div>
-            </div>
-        </div> -->
-        <img src="https://api-assets.clashroyale.com/cards/300/W4Hmp8MTSdXANN8KdblbtHwtsbt0o749BbxNqmJYfA8.png"
-            class="card-img-top border-bottom border-2 pb-2 pt-2" alt="...">
+        <img :src="card.Card.iconUrl"
+            class="card-img-top border-bottom border-2 pb-2 pt-2" :alt="card.Card.name">
         <div class="card-body">
-            <h3 class="card-title d-flex justify-content-center pt-3">Archer</h3>
+            <h3 class="card-title d-flex justify-content-center pt-3">{{ card.Card.name }}</h3>
+            <div
+            @click.prevent="removeCard(card.CardId)"
+             class="btn btn-warning mt-4 d-flex justify-content-center">
+                Remove
+            </div>
         </div>
     </div>
 
