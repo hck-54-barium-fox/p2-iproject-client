29c68b2fbd3ad25822abb1c38e245bc2d78a6efc
Hamzah Diza Santoso <hamzah.diza@gmail.com>
2023-02-08 09:46:38 +0700

    DetailPage

---
+++ client/src/views/DetailPage.vue
@@ -0,0 +1,41 @@
+<script>
+import { useAppStore } from "../stores/appStore";
+import { mapActions, mapState } from "pinia";
+import Navbar from "../components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+  computed: {
+    ...mapState(useAppStore, ["postDataDetail"]),
+  },
+
+  methods: {
+    ...mapActions(useAppStore, ["fetchPostDetail"]),
+  },
+
+  created() {
+    this.fetchPostDetail(this.$route.params.id);
+  },
+};
+</script>
+
+<template>
+  <Navbar />
+  <section>
+    <div class="row mb-5">
+      <div class="mb-3 col-12 mb-3 mx-auto d-block mt-5" style="max-width: 1200px">
+        <div class="row g-0 justify-content-center">
+          <div class="card" style="width: 30rem">
+            <img :src="postDataDetail.imgUrl" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <p class="card-text" style="font-weight: bold">{{ postDataDetail.title }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
