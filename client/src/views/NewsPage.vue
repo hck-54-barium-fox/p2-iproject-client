4b2522172f0a49b8826c0845126796756efbe442
eduardusoldi <eduardusoldi98@gmail.com>
2023-02-08 15:03:15 +0700

    dev

--- client/src/views/NewsPage.vue
+++ client/src/views/NewsPage.vue
@@ -44,7 +44,7 @@ export default {
                 <div class="d-block w-100 shadow p-5 mb-5 rounded d-flex justify-content-center">
                     <iframe width="1280" height="720" src="https://www.youtube.com/embed/XWI5qf3fFUk"
                         title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                         allowfullscreen></iframe>
                 </div>
             </div>
